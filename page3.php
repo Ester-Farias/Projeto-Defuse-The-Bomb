7cacbbc15e9ea0ca6f7add54595b1eef66fb3d28
Ester Souza <esterfariasdesouza19@gmail.com>
2024-02-09 08:25:34 -0300

    logica das tentativas

--- page3.php
+++ page3.php
@@ -6,34 +6,33 @@ if (!isset($_SESSION['bomb_number'])) {
     $_SESSION['bomb_number'] = rand(1, 10); // Altere o intervalo conforme necessário
     $_SESSION['attempts'] = 3;
 }
-print_r($_SESSION['bomb_number']);
-print_r($_SESSION['attempts']);
+//print_r($_SESSION['bomb_number']);
+//print_r($_SESSION['attempts']);
 // Verifica se o formulário foi enviado
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Decrementa o número de tentativas
     $_SESSION['attempts']--;
 
     // Obtém o palpite do usuário
-    $user_guess = $_POST['guess'];
+    $user_guess = $_POST['number_bomb_user'];
 
     // Verifica se o palpite é correto
-    if ($user_guess == $_SESSION['bomb_number'] && $_SESSION['attempts'] > 0) {
-        header("Location: end.html");
-        exit();
+    if ($user_guess == $_SESSION['bomb_number'] and $_SESSION['attempts'] > 0) {
+       unset($_SESSION['attempts']);
+       session_destroy();
+       header("Location: end.html");
+       exit();
     } else if ($_SESSION['attempts'] == 0 ){
-      header("location: gameover.html");
-    }
-
-    // Verifica se o usuário ainda tem tentativas
-    if ($_SESSION['attempts'] > 0) {
+       unset($_SESSION['attempts']);
+       session_destroy();
+       header("location: gameover.html");
+       exit();
+    }else {
         // Redireciona de volta para a página principal com o número de tentativas restantes
         header("Location: page3.php?attempts=" . $_SESSION['attempts']);
         exit();
-    } else {
-        // Se o usuário não tiver mais tentativas, redireciona para a página de game over
-        header("Location: gameover.html");
-        exit();
-    }
+    
+}
 }
 
 // Verifica se o número de tentativas está definido
